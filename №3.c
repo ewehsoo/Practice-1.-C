f34e48a7d3bdf2324666c8704060658fadffe00c
лисс <samuranovaelizaveta@gmail.com>
2025-03-19 21:05:32 +0300

    Update and rename №3.ess to №3.c

--- №3.c
+++ №3.c
@@ -2,9 +2,9 @@
 int main() {
 int a, b, c;
 printf ("Введите 3 числа = ");
-scanf ("%d %d %d", &a, &b, &c);
+scanf ("%d %d %d",&a,&b,&c);
 int result = a*b*c;
-int sign = (result > 0) - (result < 0);
+int sign = (result>0)-(result<0);
 printf ("%d\n", sign);
   return 0;
   }
