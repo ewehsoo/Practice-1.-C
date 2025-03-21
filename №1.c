e4c684e2bd36f3a0f5534c93f6cfb80599b5cfe3
лисс <samuranovaelizaveta@gmail.com>
2025-03-21 10:51:58 +0300

    Update №1.c

--- №1.c
+++ №1.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-int main() 
-
-{
-float temperature;
-printf ( "Напишите температуру в Цельсия: \n" );
-scanf ( "%f", &temperature );
-printf ( "C= %.2f ;", temperature );
-float farengate;
-farengate = ( temperature*9.0/5.0 ) + 32;
-printf ( "F= %.2f", farengate, "\n" );
-  return 0;
- }
+int main() {
+ float temperature;
+  
+ printf( "Напишите температуру в Цельсия: \n" );
+ scanf( "%f", &temperature );
+ printf( "C = %.2f ;", temperature );
+  
+ float farengate;
+  
+ farengate = ( temperature * 9.0 / 5.0 ) + 32;
+  
+ printf( "F = %.2f", farengate, "\n" );
+  
+ return 0;
+}
