6521c61046552bebd47d18ca1419d96338e0db7b
лисс <samuranovaelizaveta@gmail.com>
2025-03-21 08:44:00 +0300

    Update №3.c

--- №3.c
+++ №3.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-int main() {
+int main() 
+
+{
 int a, b, c;
-printf ("Введите 3 числа = ");
-scanf ("%d %d %d",&a,&b,&c);
+printf ( "Введите 3 числа = " );
+scanf ( "%d %d %d",&a,&b,&c );
 int result = a*b*c;
-int sign = (result>0)-(result<0);
-printf ("%d\n", sign);
-  return 0;
-  }
+int sign = ( result>0 ) - ( result<0 );
+printf ( "%d\n", sign );
+return 0;
+}
