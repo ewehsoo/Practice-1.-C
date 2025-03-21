06dc0c898a773ad9cb472d7c170b45f0377a68a8
лисс <samuranovaelizaveta@gmail.com>
2025-03-21 10:53:13 +0300

    Update №2.c

--- №2.c
+++ №2.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
-int main() 
 
-{
-int num;
-printf( "Напишите целое число: \n" );
-scanf( "%d", &num );
-printf( "Ваше число:%d; ", num );
-int i;
-num = num % 2;
-bool chetnost = ( num == 0 );
-const char* result = chetnost ? "Число четное" : "Число нечетное";
-printf( "%s\n", result );
-return 0;
+int main() {
+ int num;
+  
+ printf( "Напишите целое число: \n" );
+ scanf( "%d", &num );
+ printf( "Ваше число:%d; ", num );
+  
+ int i;
+  
+ num = num % 2;
+ bool chetnost = ( num == 0 );
+  
+ const char* result = chetnost ? "Число четное" : "Число нечетное";
+  
+ printf( "%s\n", result );
+ return 0;
 }
