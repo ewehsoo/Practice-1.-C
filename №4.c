c7623db042f476095936ceac16bd32864cbf897c
лисс <samuranovaelizaveta@gmail.com>
2025-03-21 10:57:02 +0300

    Update №4.c

--- №4.c
+++ №4.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main() 
 
-{
-int num;
-printf ( "Введите число = " );
-scanf ( "%d", &num );
-printf ( "%d\n", (num>0) && !( num & (num-1) ));
+int main() {
+ int num;
+ printf( "Введите число = " );
+ scanf( "%d", &num );
+ printf( "%d\n", (num > 0) && !( num & (num - 1) ));
 return 0;
 }
